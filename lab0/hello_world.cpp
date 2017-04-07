7a5ce58f29f65dcba9c69334cb18e6b236b7efd1
Sidharth Gilela <sgilela@ucsc.edu>
2017-04-06 19:39:04 -0700

    Briefly turned in assignment 0

---
+++ lab0/hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "hello_world.h"
+
+void print_hello(){
+	std::cout << "Hello World!" << std::endl;
+}
+
+int main(){
+	print_hello();
+}
---
+++ lab0/hello_world.h
@@ -0,0 +1,6 @@
+#ifndef hello_world
+#define hello_world
+
+void printf_hello();
+
+#endif 
